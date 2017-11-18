5d4f54328f88f7284f8af3deda6a6f738737f588
karenpaola <33790038+karenpaola@users.noreply.github.com>
2017-11-18 15:25:05 -0400

    segundo commit

--- JavaApplication20/src/javaapplication20/Figura.java
+++ JavaApplication20/src/javaapplication20/Figura.java
@@ -9,15 +9,6 @@ package javaapplication20;
  *
  * @author LabB8
  */
-public class JavaApplication20 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        System.out.println("hola mundo ");
-    }
+public class Figura {
     
 }
